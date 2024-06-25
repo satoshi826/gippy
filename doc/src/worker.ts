76387b0c2e541b6aef8ac103329420a2ada597e8
satoshi hata <stosto826@gmail.com>
2024-06-25 23:14:02 +0900

    wip

--- doc/src/worker.ts
+++ doc/src/worker.ts
@@ -1,30 +1,17 @@
-import {main as helloTriangle} from './examples/helloTriangle/main'
-import {main as attributes} from './examples/attributes/main'
-import {main as uniforms} from './examples/uniforms/main'
-import {main as animation} from './examples/animation/main'
-import {main as resize} from './examples/resize/main'
-import {main as texture} from './examples/texture/main'
-import {main as gpgpu} from './examples/gpgpu/main'
-import {main as _3d} from './examples/3d/main'
-import {main as instancing} from './examples/instancing/main'
-import {main as frameBuffer} from './examples/frameBuffer/main'
-import {main as demo} from './examples/demo/main'
-import {main as demo0} from './examples/demo0/main'
 import {setState} from 'glaku'
 
 export const srcRecord = {
-  helloTriangle,
-  attributes,
-  uniforms,
-  animation,
-  resize,
-  texture,
-  frameBuffer,
-  '3d': _3d,
-  instancing,
-  gpgpu,
-  demo,
-  demo0
+  helloTriangle: () => import('./examples/helloTriangle/main'),
+  attributes   : () => import('./examples/attributes/main'),
+  uniforms     : () => import('./examples/uniforms/main'),
+  animation    : () => import('./examples/animation/main'),
+  resize       : () => import('./examples/resize/main'),
+  texture      : () => import('./examples/texture/main'),
+  frameBuffer  : () => import('./examples/frameBuffer/main'),
+  '3d'         : () => import('./examples/3d/main'),
+  instancing   : () => import('./examples/instancing/main'),
+  gpgpu        : () => import('./examples/gpgpu/main'),
+  demo         : () => import('./examples/demo/main')
 } as const
 
 type SrcType = keyof typeof srcRecord
@@ -38,7 +25,10 @@ onmessage = async({data}) => {
   const {canvas : offscreenCanvas, pixelRatio, src, state} = data
   if (offscreenCanvas) canvas = offscreenCanvas as OffscreenCanvas
   if (pixelRatio) pr = pixelRatio
-  if (src) srcRecord[src as SrcType](canvas!, pr)
+  if (src) {
+    const {main} = await srcRecord[src as SrcType]()
+    main(canvas!, pr)
+  }
   if (state) setState(state)
 }
 
