81f637b6ff1c41b0401b17a705eda7a1be2e24fc
satoshi hata <stosto826@gmail.com>
2024-06-27 00:11:03 +0900

    wip

--- doc/src/examples/demo/Page.tsx
+++ doc/src/examples/demo/Page.tsx
@@ -111,6 +111,7 @@ const Wrapper = ({post, children}: {post : (any: object) => void, children : Rea
       display='flex'
       width='100%'
       height='100%'
+      sx={{touchAction: 'none', cursor: 'pointer', backgroundColor: 'red'}}
       onMouseMove={handleMouseMove}
       onMouseDown={handleMouseDown}
       onMouseUp={handleMouseUp}
