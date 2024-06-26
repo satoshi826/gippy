5cd4e396909d5ba140d229a54864181bc03cf47b
satoshi hata <stosto826@gmail.com>
2024-06-26 23:41:49 +0900

    wip

--- doc/src/worker.ts
+++ doc/src/worker.ts
@@ -1,19 +1,29 @@
+import {main as helloTriangle} from './examples/helloTriangle/main'
+import {main as attributes} from './examples/attributes/main'
+import {main as uniforms} from './examples/uniforms/main'
+import {main as animation} from './examples/animation/main'
+import {main as resize} from './examples/resize/main'
+import {main as texture} from './examples/texture/main'
+import {main as gpgpu} from './examples/gpgpu/main'
+import {main as _3d} from './examples/3d/main'
+import {main as instancing} from './examples/instancing/main'
+import {main as frameBuffer} from './examples/frameBuffer/main'
+import {main as demo} from './examples/demo/main'
 import {setState} from 'glaku'
 
 export const srcRecord = {
-  helloTriangle: () => import('./examples/helloTriangle/main'),
-  attributes   : () => import('./examples/attributes/main'),
-  uniforms     : () => import('./examples/uniforms/main'),
-  animation    : () => import('./examples/animation/main'),
-  resize       : () => import('./examples/resize/main'),
-  texture      : () => import('./examples/texture/main'),
-  frameBuffer  : () => import('./examples/frameBuffer/main'),
-  '3d'         : () => import('./examples/3d/main'),
-  instancing   : () => import('./examples/instancing/main'),
-  gpgpu        : () => import('./examples/gpgpu/main'),
-  demo         : () => import('./examples/demo/main')
+  helloTriangle,
+  attributes,
+  uniforms,
+  animation,
+  resize,
+  texture,
+  frameBuffer,
+  '3d': _3d,
+  instancing,
+  gpgpu,
+  demo
 } as const
-
 type SrcType = keyof typeof srcRecord
 
 console.log('starting worker')
@@ -26,8 +36,7 @@ onmessage = async({data}) => {
   if (offscreenCanvas) canvas = offscreenCanvas as OffscreenCanvas
   if (pixelRatio) pr = pixelRatio
   if (src) {
-    const {main} = await srcRecord[src as SrcType]()
-    main(canvas!, pr)
+    srcRecord[src as SrcType](canvas!, pr)
   }
   if (state) setState(state)
 }
