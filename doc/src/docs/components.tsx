c63c38f61f4ab1099dcdc58ece5fb0d1e405ba0f
satoshi hata <stosto826@gmail.com>
2024-07-02 00:00:55 +0900

    wip

--- doc/src/docs/components.tsx
+++ doc/src/docs/components.tsx
@@ -1,7 +1,24 @@
-import {Typography, styled} from '@mui/material'
-
+import {Divider, Typography, styled} from '@mui/material'
+import {Light as SyntaxHighlighter} from 'react-syntax-highlighter'
+import js from 'react-syntax-highlighter/dist/esm/languages/hljs/javascript'
+import bash from 'react-syntax-highlighter/dist/esm/languages/hljs/bash'
+import {tomorrowNight} from 'react-syntax-highlighter/dist/esm/styles/hljs'
+import {CSSProperties, useMemo} from 'react'
 
 const BodyTextTypography = styled(Typography)(({theme}) => ({color: theme.palette.grey[300]}))
+
+export function TitleText({children}: React.PropsWithChildren) {
+  return (
+    <>
+      <Typography variant='h3' >{children}</Typography>
+      <Divider sx={{mt: 2}}/>
+    </>
+  )
+}
+
+export function SubTitleText({children}: React.PropsWithChildren) {
+  return <Typography variant='h4' sx={{mt: 4, mb: 2}}>{children}</Typography>
+}
 
 export function BodyText({children}: React.PropsWithChildren) {
   return (
@@ -9,5 +26,25 @@ export function BodyText({children}: React.PropsWithChildren) {
       {children}
     </BodyTextTypography>
   )
+}
 
+const useSyntaxHighlighter = () => useMemo(() => {
+  SyntaxHighlighter.registerLanguage('js', js)
+  SyntaxHighlighter.registerLanguage('bash', bash)
+  return SyntaxHighlighter
+}, [])
+
+const syntaxHighlighterStyle: CSSProperties = {
+  border      : '1px solid #555',
+  borderRadius: '12px',
+  padding     : '8px 16px 8px 16px'
+}
+
+export function Syntax({children, lang = 'js'}: {children: string, lang?: 'js' | 'bash'}) {
+  const SyntaxHighlighter = useSyntaxHighlighter()
+  return (
+    <SyntaxHighlighter language={lang} style={tomorrowNight} customStyle={syntaxHighlighterStyle}>
+      {children}
+    </SyntaxHighlighter>
+  )
 }
--- doc/src/docs/introduction/Page.tsx
+++ doc/src/docs/introduction/Page.tsx
@@ -1,23 +1,21 @@
-import {Container, Divider, Stack, Typography} from '@mui/material'
-import {BodyText} from '../components'
-import SyntaxHighlighter from 'react-syntax-highlighter'
-import {tomorrowNight} from 'react-syntax-highlighter/dist/esm/styles/hljs'
+import {Container, Stack, Typography} from '@mui/material'
+import {BodyText, SubTitleText, Syntax, TitleText} from '../components'
 
 export default function Page() {
-  const codeString = '(num) => num + 1'
   return (
     <Stack flexDirection='row' justifyContent='center' flexGrow={1}>
       <Container sx={{mx: 1, my: 2}}>
-        <Typography variant='h3' >Introduction</Typography>
-        <Divider sx={{my: 1}}/>
-        <Typography variant='h4' sx={{mb: 1}}>Overview</Typography>
+        <TitleText >Introduction</TitleText>
+        <SubTitleText >Overview</SubTitleText>
         <BodyText >
           Glaku はシンプルかつミニマムなwebGLライブラリで、ピュアなwebGLが持つパワーを引き出すことを目的にしています。Enjoy WebGL !
         </BodyText>
-        <Typography variant='h4' sx={{mt: 4}}>Installation</Typography>
-        <SyntaxHighlighter language="typescript" style={tomorrowNight}>
-          {codeString}
-        </SyntaxHighlighter>
+        <SubTitleText>Installation</SubTitleText>
+        <Syntax lang='bash'>
+          npm i glaku
+        </Syntax>
+        <SubTitleText>Quick Start</SubTitleText>
+        <Typography variant='h5' sx={{mt: 4, mb: 2}}>Vanilla</Typography>
       </Container>
     </Stack>
 
